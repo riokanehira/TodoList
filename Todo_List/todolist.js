57194ef84ee846c66a07c9314433ce3d4620fccf
riokanehira <rio.kanehira@devpls.edu.vn>
2025-08-15 19:59:10 +0700

    change css

---
+++ Todo_List/todolist.js
@@ -0,0 +1,8 @@
+//idの引用と定義
+const todoWrite =document.getElementById('todo-write');
+const todoSubmit =document.getElementById('todo-submit');
+
+//Addボタンを押したらテキストボックス内の内容をliに追加
+//チェックボタンを押したら色が薄くなる、線を引く
+//ばつボタンを押したら該当の項目を消す
+//リセットボタンを押したら全部消える
